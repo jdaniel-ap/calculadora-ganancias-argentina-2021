7f58e62d9275baa1ec52953ce92a48ecab361222
jdaniel-ap <74429277+jdaniel-ap@users.noreply.github.com>
2021-05-25 16:53:52 -0300

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,89 @@
+const userInput = document.getElementById("input-value");
+const taxesData = [
+  {
+    min: 0,
+    max: 47669.16,
+    fixedTaxes: 0,
+    variabelTaxes: 5,
+  },
+  {
+    min: 47669.17,
+    max: 95338.32,
+    fixedTaxes: 2383.46,
+    variabelTaxes: 9,
+  },
+  {
+    min: 95338.33,
+    max: 143007.48,
+    fixedTaxes: 6673.68,
+    variabelTaxes: 12,
+  },
+  {
+    min: 143007.49,
+    max: 190676.65,
+    fixedTaxes: 12393.98,
+    variabelTaxes: 15,
+  },
+  {
+    min: 190676.66,
+    max: 286014.86,
+    fixedTaxes: 19544.36,
+    variabelTaxes: 19,
+  },
+  {
+    min: 286014.87,
+    max: 381353.28,
+    fixedTaxes: 37658.64,
+    variabelTaxes: 23,
+  },
+  {
+    min: 381353.29,
+    max: 572029.92,
+    fixedTaxes: 59586.45,
+    variabelTaxes: 27,
+  },
+  {
+    min: 572029.93,
+    max: 762706.57,
+    fixedTaxes: 111069.14,
+    variabelTaxes: 31,
+  },
+  {
+    min: 762706.57,
+    max: Infinity,
+    fixedTaxes: 170178.9,
+    variabelTaxes: 35,
+  },
+];
+
+function formattNumber(value) {
+    return (value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"));
+}
+
+function getRow() {
+  const calculebtn = document.getElementById("calcule");
+
+  calculebtn.addEventListener("click", () => {
+    const row = taxesData.findIndex(
+      (element) =>
+        userInput.value >= element.min && userInput.value <= element.max
+    );
+    setTaxes(row);
+  });
+}
+
+function setTaxes(index) {
+  const { min, fixedTaxes, variabelTaxes } = taxesData[index];
+  const percentajeTax = document.getElementById("variabel-tax");
+  const totalVariabelTax = document.getElementById("variabel-total");
+  const fixedTax = document.getElementById("fixed-tax");
+  const total = document.getElementById("total");
+  let variabelCalc = (variabelTaxes / 100) * (userInput.value - min);
+
+  percentajeTax.textContent = `${variabelTaxes}%`;
+  totalVariabelTax.textContent = formattNumber(variabelCalc);
+  fixedTax.textContent = formattNumber(fixedTaxes);
+  total.textContent = `$ ${formattNumber(variabelCalc + fixedTaxes)}`;
+}
+
+getRow();
